30ab28fede758e272cff23fef388ae2c5b29a9ad
Ben Strachan <ben@ownerhealth.com.au>
2018-02-11 18:57:25 +1000

    camper

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,8 +1,9 @@
 class VehiclesController < ApplicationController
+  layout "visitors"
   def index
     @vehicles = Vehicle.ransack(params[:q])
                        .result
-                       
+
     render 'index', layout: false
   end
 end
